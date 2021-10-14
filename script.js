cd38a1bdfb9e13d67c82c8fea0436a5c2bfc3cd2
Simran Sandhu <simran7s@my.yorku.ca>
2021-10-14 00:01:40 -0400

    complete project

---
+++ script.js
@@ -0,0 +1,17 @@
+const key = document.querySelector(".key")
+const keyCode = document.querySelector(".keyCode")
+const code = document.querySelector(".code")
+
+const container = document.querySelector(".container")
+
+
+
+window.addEventListener("keydown", (e) => {
+    console.log(e);
+
+    container.classList.add("active");
+
+    key.innerHTML = e.code;
+    keyCode.innerHTML = e.keyCode;
+    code.innerHTML = e.code;
+})
